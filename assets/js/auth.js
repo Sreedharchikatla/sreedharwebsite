c80e605cecd70c5ef50747332ec231389d9d98cb
Sreedhar Chikatla <sridharcheekatla@hotmail.com>
2024-12-25 17:34:42 -0600

    added signup

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -10,3 +10,9 @@ document.getElementById('azure-b2c-btn').addEventListener('click', function () {
     const authUrl = `${authority}/oauth2/v2.0/authorize?p=B2C_1_signupsignin&client_id=${clientId}&nonce=${nonce}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope}&response_type=${responseType}&prompt=${prompt}`;
     window.location.href = authUrl;
 });
+// Sign Up link handler
+document.getElementById('signup-link').addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent default link behavior
+    const signupUrl = `${authority}/oauth2/v2.0/authorize?p=B2C_1_signupsignin&client_id=${clientId}&nonce=${nonce}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope}&response_type=${responseType}&prompt=${prompt}`;
+    window.location.href = signupUrl; // Redirect to the signup flow
+});
